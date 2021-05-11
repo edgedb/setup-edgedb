6c0910d312b6e602254fae7393f7101ea2960b7f
fmoor <fmoor@gmx.com>
2021-05-11 10:32:46 -0700

    add support for windows when WSL is available
    The approach used here has some caveats:
    
    - the user must have installed WSL
    - The user must have installed ca-certificates or similar depending on
      the linux distribution they installed in WSL.
    - The binaries `edgedb` and `edgedb-server` are available in the linux
      subsystem path not in the windows path.
    - The default linux user configured by Vampire/setup-wsl@v1 is root
      which causes `edgedb-server` to fail.
    - Currently `edgedb server` commands fail. I haven't debugged this
      yet.

---
+++ src/win.ts
@@ -0,0 +1,92 @@
+import * as os from 'os'
+import * as fs from 'fs'
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as main from './main'
+import * as fetch from 'node-fetch'
+
+export async function run(): Promise<void> {
+  try {
+    await installCLI()
+    await installServer()
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+async function checkOutput(cmd: string, args?: string[]): Promise<string> {
+  let out = ''
+
+  const options = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        out += data.toString()
+      }
+    }
+  }
+
+  await exec.exec(cmd, args, options)
+  return out.trim()
+}
+
+async function getBaseDist(): Promise<string> {
+  const arch = os.arch()
+  const platform = (await checkOutput('wsl uname')).toLocaleLowerCase()
+
+  return main.getBaseDist(arch, platform)
+}
+
+async function installCLI(): Promise<void> {
+  const requestedCLIVersion = core.getInput('cli-version')
+  const arch = os.arch()
+  const includeCliPrereleases = true
+  let cliVersionRange = '*'
+  let dist = await getBaseDist()
+
+  if (requestedCLIVersion === 'nightly') {
+    dist += '.nightly'
+  } else if (requestedCLIVersion !== 'stable') {
+    cliVersionRange = requestedCLIVersion
+  }
+
+  const versionMap = await main.getVersionMap(dist)
+  const matchingVer = await main.getMatchingVer(
+    versionMap,
+    cliVersionRange,
+    includeCliPrereleases
+  )
+  const cliPkg = versionMap.get(matchingVer)
+  const downloadUrl = new URL(cliPkg.installref, main.EDGEDB_PKG_ROOT).href
+
+  core.info(
+    `Downloading edgedb-cli ${matchingVer} - ${arch} from ${downloadUrl}`
+  )
+
+  const rsp = await fetch.default(downloadUrl)
+  const file = fs.createWriteStream('C:\\Temp\\edgedb-cli')
+  rsp.body.pipe(file)
+
+  await checkOutput('wsl cp /mnt/c/temp/edgedb-cli /usr/bin/edgedb')
+}
+
+async function installServer(): Promise<void> {
+  const requestedVersion = core.getInput('server-version')
+
+  const args = ['edgedb', 'server', 'install', '--method', 'package']
+
+  if (requestedVersion === 'nightly') {
+    args.push('--nightly')
+  } else if (requestedVersion !== '' && requestedVersion !== 'stable') {
+    args.push('--version')
+    args.push(requestedVersion)
+  }
+
+  await checkOutput('wsl', args)
+  const bin = await checkOutput('wsl ls /usr/bin/edgedb-server-*')
+
+  if (bin === '') {
+    throw Error('could not find edgedb-server bin')
+  }
+
+  await checkOutput('wsl', ['ln', '-s', bin.trim(), '/usr/bin/edgedb-server'])
+}
